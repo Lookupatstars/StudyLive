c2858f27b5701bbbd44cb0c7bcef22adb1686b82
Lookupatstars <g50shj@163.com>
2020-03-18 16:34:15 +0800

    注册跳转

--- app/src/main/java/com/aaron/studylive/LoginStudentActivity.java
+++ app/src/main/java/com/aaron/studylive/LoginStudentActivity.java
@@ -3,11 +3,14 @@ package com.aaron.studylive;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginStudentActivity extends AppCompatActivity {
 
@@ -17,13 +20,14 @@ public class LoginStudentActivity extends AppCompatActivity {
     private TextView tv_forgotpasswd;
     private Button btn_login,btn_regist;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_student);
         //透明状态栏
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
 
         et_student_user = findViewById(R.id.et_student_user);
         et_student_passwd = findViewById(R.id.et_student_passwd);
@@ -32,6 +36,11 @@ public class LoginStudentActivity extends AppCompatActivity {
 
         btn_login = findViewById(R.id.btn_login);
         btn_regist = findViewById(R.id.btn_regist);
+
+        // 给用户名编辑框添加文本变化监听器
+//        et_student_user.addTextChangedListener(new JumpTextWatcher(et_student_user, et_student_passwd));
+        // 给密码编辑框添加文本变化监听器
+//        et_student_passwd.addTextChangedListener(new JumpTextWatcher(et_student_passwd, btn_login));
 
         btn_login.setOnClickListener(new LoginOnClickListener());
         btn_regist.setOnClickListener(new RegistClickListener());
@@ -44,6 +53,10 @@ public class LoginStudentActivity extends AppCompatActivity {
         @Override
         public void onClick(View v){
             if(v.getId()==R.id.btn_login){
+                if(et_student_user==null){
+//                    Toast.makeText();
+                }
+                //判断user和password
                 Intent intent = new Intent(LoginStudentActivity.this,MainActivity.class);
                 startActivity(intent);
                 LoginStudentActivity.this.finish();
@@ -67,6 +80,49 @@ public class LoginStudentActivity extends AppCompatActivity {
     }
 
 
+    // 定义一个编辑框监听器，在输入回车符时自动跳到下一个控件
+//    private class JumpTextWatcher implements TextWatcher {
+//        private EditText mThisView;  // 声明当前的编辑框对象
+//        private View mNextView; // 声明下一个视图对象
+//
+//        public JumpTextWatcher(EditText vThis, View vNext) {
+//            super();
+//            mThisView = vThis;
+//            if (vNext != null) {
+//                mNextView = vNext;
+//            }
+//        }
+//
+//        // 在编辑框的输入文本变化前触发
+//        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+//
+//        // 在编辑框的输入文本变化时触发
+//        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+//
+//        // 在编辑框的输入文本变化后触发
+//        public void afterTextChanged(Editable s) {
+//            String str = s.toString();
+//            // 发现输入回车符或换行符
+//            if (str.contains("\r") || str.contains("\n")) {
+//                // 去掉回车符和换行符
+//                mThisView.setText(str.replace("\r", "").replace("\n", ""));
+//                if (mNextView != null) {
+//                    // 让下一个视图获得焦点，即将光标移到下个视图
+//                    mNextView.requestFocus();
+//                    // 如果下一个视图是编辑框，则将光标自动移到编辑框的文本末尾
+//                    if (mNextView instanceof EditText) {
+//                        EditText et = (EditText) mNextView;
+//                        // 让光标自动移到编辑框内部的文本末尾
+//                        // 方式一：直接调用EditText的setSelection方法
+//                        et.setSelection(et.getText().length());
+//                        // 方式二：调用Selection类的setSelection方法
+//                        //Editable edit = et.getText();
+//                        //Selection.setSelection(edit, edit.length());
+//                    }
+//                }
+//            }
+//        }
+//    }
 
 
 }
--- app/src/main/java/com/aaron/studylive/RegistActivity.java
+++ app/src/main/java/com/aaron/studylive/RegistActivity.java
@@ -1,25 +1,41 @@
 package com.aaron.studylive;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RegistActivity extends AppCompatActivity {
 
-    private Button btn_reg_back,btn_reg_check,btn_reg_reg,btn_reg_pro;
+    private Button btn_reg_back,btn_reg_check,btn_reg_reg;
     private EditText et_reg_phone,et_reg_check,et_reg_name,et_reg_passwd,et_reg_conpasswd;
+    private TextView tv_reg_pro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
 
+        //事件注册的类
+        initView();
+        btn_reg_back.setOnClickListener(new BackLogin());
+        tv_reg_pro.setOnClickListener(new ProText());
+
+    }
+
+    //事件注册的类
+    private void initView(){
         //Button按钮的事件注册
         btn_reg_back = findViewById(R.id.btn_reg_back);
         btn_reg_check = findViewById(R.id.btn_reg_check);
         btn_reg_reg = findViewById(R.id.btn_reg_reg);
-        btn_reg_pro = findViewById(R.id.btn_reg_pro);
+        tv_reg_pro = findViewById(R.id.tv_reg_pro);
 
         //EditText的事件注册
         et_reg_phone = findViewById(R.id.et_reg_phone);
@@ -27,9 +43,25 @@ public class RegistActivity extends AppCompatActivity {
         et_reg_name = findViewById(R.id.et_reg_name);
         et_reg_passwd = findViewById(R.id.et_reg_passwd);
         et_reg_conpasswd = findViewById(R.id.et_reg_conpasswd);
+    }
 
 
+    private class BackLogin implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if(v.getId()==R.id.btn_reg_back){
+                Intent intent = new Intent(RegistActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
 
-
+    private class ProText implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //点击服务协议的显示内容
+            Toast.makeText(RegistActivity.this,"您点击了服务条约",Toast.LENGTH_SHORT).show();
+        }
     }
 }
